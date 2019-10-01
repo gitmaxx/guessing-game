c9c445161a18da980309860eb91737b1d91237f4
Akulinichev Maxim <mxmrus@gmail.com>
2019-10-01 17:34:13 +0300

    feat: implementaion of guessing game class, text completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min+1;
+        this.max = max-1;
+        
+       
     }
 
     guess() {
+        
+        return Math.ceil((this.max+this.min)/2);
 
     }
 
     lower() {
-
+        this.max = Math.ceil((this.max+this.min)/2)-1;
     }
 
     greater() {
-
+        this.min = Math.ceil((this.max+this.min)/2)+1;
     }
 }
 
